ac4ad7b12c837ada669bec9350f6610add85c762
Olamidehash1234 <olamideadeola2005@gmail.com>
2025-05-06 04:06:57 +0100

    text changed in the project page

--- src/pages/projects/Header.tsx
+++ src/pages/projects/Header.tsx
@@ -3,14 +3,17 @@ const ProductsHeader = () => {
     <div className="w-full  px-[16px] md:px-[80px] py-8 md:py-[80px] flex flex-col md:flex-row items-start md:items-center justify-between">
       <div className="">
         <div className="mb-2 md:mb-[10px]">
-          <p className="text-[#E25319] font-medium text-[14px] md:text-[16px] uppercase tracking-wide md:leading-[30px]">PRODUCTS</p>
+          <p className="text-[#E25319] font-medium text-[14px] md:text-[16px] uppercase tracking-wide md:leading-[30px]">
+          PROJECTS
+          </p>
         </div>
         <h1 className="text-[24px] md:text-[40px] font-medium text-[#001D0D] mb-4 leading-tight md:leading-[50px] md:mb-[48px]">
-          Products That Drive Innovation Across Africa
+          Projects That Power Progress Across Africa
         </h1>
         <p className="text-sm md:text-[14px] text-[#001D0D] md:max-w-[766px] md:leading-[30px]">
-          Please find below, our list of products. Our product listing changes every year to accommodate new innovation from our 
-          manufacturing department. Feel free to bookmark this page and check often for updates!
+          MicCom Cables and Wires Ltd has been engaged in the manufacturing and
+          supply of cables and wires for various projects across Nigeria and
+          Africa as whole. Here are just some of the projects;
         </p>
       </div>
 
